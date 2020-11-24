4c79c213a8ab63e78f954e7a828a7e08d4507da9
tqk2811 <tqk2811@gmail.com>
2020-11-25 00:15:17 +0700

    update

---
+++ TqkLibrary/Selenium/SeleniumHelper.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TqkLibrary.Selenium
+{
+  public static class SeleniumHelper
+  {
+    public static void JsClick(this IWebElement webElement, ChromeDriver drive)
+    {
+      drive.ExecuteScript("arguments[0].click();", webElement);
+    }
+    public static void JsClick(this ChromeDriver drive,IWebElement webElement)
+    {
+      drive.ExecuteScript("arguments[0].click();", webElement);
+    }
+
+    public static void JsWriteText(this IWebElement webElement, ChromeDriver drive, string text)
+    {
+      drive.ExecuteScript($"arguments[0].value = \"{text}\";", webElement);
+    }
+    public static void JsWriteText(this ChromeDriver drive, IWebElement webElement,  string text)
+    {
+      drive.ExecuteScript($"arguments[0].value = \"{text}\";", webElement);
+    }
+  }
+}

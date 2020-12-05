31688306ac12753d3c2f821d99c82ef54ab23beb
tqk2811 <tqk2811@gmail.com>
2020-12-05 19:42:00 +0700

    selenium chrome extension

---
+++ TqkLibrary.SeleniumSupport/ProxyLoginExtension.cs
@@ -0,0 +1,36 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.IO;
+using System.Text;
+
+namespace TqkLibrary.SeleniumSupport
+{
+  public static class ProxyLoginExtension
+  {
+    const string manifest_json = "{\"version\":\"1.0.0\",\"manifest_version\":2,\"name\":\"ChromeProxy\",\"permissions\":[\"proxy\",\"tabs\",\"unlimitedStorage\",\"storage\",\"<all_urls>\",\"webRequest\",\"webRequestBlocking\"],\"background\":{\"scripts\":[\"background.js\"]},\"minimum_chrome_version\":\"22.0.0\"}";
+    const string background_js = "var config={mode:\"fixed_servers\",rules:{singleProxy:{scheme:\"http\",host:\"{host}\",port:{port}},bypassList:[\"localhost\"]}};chrome.proxy.settings.set({value:config,scope:\"regular\"},function(){});functioncallbackFn(details){return{authCredentials:{username:\"{username}\",password:\"{password}\"}};}chrome.webRequest.onAuthRequired.addListener(callbackFn,{urls:[\"<all_urls>\"]},['blocking']);";
+
+    public static void GenerateExtension(string filepath, string host,int port,string username, string password)
+    {
+      string background = background_js.Replace("{host}", host).Replace("{port}", port.ToString()).Replace("{username}", username).Replace("{password}", password);
+      ZipFile zipFile = ZipFile.Create(filepath);
+      zipFile.BeginUpdate();
+      using(CustomStaticDataSource staticDataSource = new CustomStaticDataSource(manifest_json)) zipFile.Add(staticDataSource, "manifest.json");
+      using (CustomStaticDataSource staticDataSource = new CustomStaticDataSource(background)) zipFile.Add(staticDataSource, "background.js");
+      zipFile.CommitUpdate();
+      zipFile.Close();
+    }
+
+  }
+
+  internal class CustomStaticDataSource : IStaticDataSource , IDisposable
+  {
+    readonly MemoryStream memoryStream;
+    public CustomStaticDataSource(string content)
+    {
+      this.memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+    }
+    public void Dispose() => memoryStream.Dispose();
+    public Stream GetSource() => memoryStream;
+  }
+}

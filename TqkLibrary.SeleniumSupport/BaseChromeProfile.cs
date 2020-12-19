f0ab1f0c75559cc7f692a831bc4deab3f1336a3f
tqk2811 <tqk2811@gmail.com>
2020-12-19 16:38:10 +0700

    Update Selenium Drop File Non Input WpfUi SaveSettingData

--- TqkLibrary.SeleniumSupport/BaseChromeProfile.cs
+++ TqkLibrary.SeleniumSupport/BaseChromeProfile.cs
@@ -12,13 +12,6 @@ using System.Threading.Tasks;
 
 namespace TqkLibrary.SeleniumSupport
 {
-  public class ChromeAutoException : Exception
-  {
-    public ChromeAutoException(string Message) : base(Message)
-    {
-    }
-  }
-
   public delegate void RunningStateChange(bool change);
 
   public abstract class BaseChromeProfile
@@ -44,7 +37,7 @@ namespace TqkLibrary.SeleniumSupport
       service.HideCommandPromptWindow = HideCommandPromptWindow;
     }
 
-    protected virtual bool OpenChrome(ChromeOptions chromeOptions)
+    public virtual bool OpenChrome(ChromeOptions chromeOptions)
     {
       if (!IsOpenChrome)
       {
@@ -56,7 +49,7 @@ namespace TqkLibrary.SeleniumSupport
       return false;
     }
 
-    protected virtual bool OpenChrome(CancellationTokenSource cancellationTokenSource, Func<ChromeOptions> chromeOptions)
+    public virtual bool OpenChrome(CancellationTokenSource cancellationTokenSource, Func<ChromeOptions> chromeOptions)
     {
       if (!IsOpenChrome)
       {
@@ -66,7 +59,7 @@ namespace TqkLibrary.SeleniumSupport
       return false;
     }
 
-    protected virtual bool OpenChromeWithoutSelenium(string Arguments, string ChromePath = null)
+    public virtual bool OpenChromeWithoutSelenium(string Arguments, string ChromePath = null)
     {
       if (!IsOpenChrome)
       {
@@ -88,7 +81,7 @@ namespace TqkLibrary.SeleniumSupport
       return false;
     }
 
-    protected virtual bool CloseChrome()
+    public virtual bool CloseChrome()
     {
       if (IsOpenChrome)
       {
@@ -105,7 +98,7 @@ namespace TqkLibrary.SeleniumSupport
       return false;
     }
 
-    protected virtual void Stop() => tokenSource?.Cancel();
+    public virtual void Stop() => tokenSource?.Cancel();
 
     protected virtual void Delay(int min, int max)
     {
@@ -289,6 +282,38 @@ namespace TqkLibrary.SeleniumSupport
         }
       }
       return null;
+    }
+
+    private const string JsDropFile = @"var target = arguments[0],
+offsetX = arguments[1],
+offsetY = arguments[2],
+document = target.ownerDocument || document,
+window = document.defaultView || window;
+
+var input = document.createElement('INPUT');
+input.type = 'file';
+input.style.display = 'none';
+input.onchange = function () {
+  var rect = target.getBoundingClientRect(),
+    x = rect.left + (offsetX || (rect.width >> 1)),
+    y = rect.top + (offsetY || (rect.height >> 1)),
+    dataTransfer = { files: this.files };
+
+  ['dragenter', 'dragover', 'drop'].forEach(function (name) {
+    var evt = document.createEvent('MouseEvent');
+    evt.initMouseEvent(name, !0, !0, window, 0, 0, 0, x, y, !1, !1, !1, !1, 0, null);
+    evt.dataTransfer = dataTransfer;
+    target.dispatchEvent(evt);
+  });
+  setTimeout(function () { document.body.removeChild(input); }, 25);
+}
+document.body.appendChild(input);
+return input;";
+
+    protected void DropFile(string file, IWebElement webElement, int offsetX, int offsetY)
+    {
+      IWebElement input = (IWebElement)chromeDriver.ExecuteScript(JsDropFile, webElement, offsetX, offsetY);
+      input.SendKeys(file);
     }
   }
 }

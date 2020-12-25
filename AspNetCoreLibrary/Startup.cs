a921df3cdd0ec711304a13dd5c2a5f16f9680e3c
tqk2811 <tqk2811@gmail.com>
2020-12-25 14:58:18 +0700

    AspNetCore

---
+++ AspNetCoreLibrary/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace AspNetCoreLibrary
+{
+  public class BaseStartup
+  {
+    public BaseStartup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+    {
+      Configuration = configuration;
+      WebHostEnvironment = webHostEnvironment;
+    }
+
+    public IWebHostEnvironment WebHostEnvironment { get; }
+    public IConfiguration Configuration { get; }
+  }
+}
